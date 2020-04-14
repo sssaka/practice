4b4f9248cbc6129203e08c7e69939ede0aaf4220
Nishino-yohei <57607029+Nishino-yohei@users.noreply.github.com>
2020-04-14 17:21:53 +0900

    Update B_nishino.py

--- B_nishino.py
+++ B_nishino.py
@@ -1,1 +1,12 @@
-
+s=input()
+a=s.split(' ')
+N=int(len(a))
+for i in range(1,N-1):
+    h=i+1
+    if int(a[h])-int(a[i])>0:
+        print('up',str(int(a[h])-int(a[i])))
+    elif int(a[h])-int(a[i])==0:
+        print('stay')
+    else:
+        print('down',str(-int(a[h])+int(a[i])))
+        
